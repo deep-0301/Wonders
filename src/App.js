6be52150d81072581409de95f654079412dc5fd7
deep-0301 <80214651+deep-0301@users.noreply.github.com>
2021-05-10 14:11:50 +0530

    searchbar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './App.css'
+import Wonders from './Wonders'
+
+const App = () => {
+
+  return (
+    <>
+      <Wonders />
+    </>
+  )
+
+}
+
+export default App
